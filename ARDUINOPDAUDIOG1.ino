c9349a29f7cfddedfa9c3f4bcf1c754b930a3ad8
yas0reo <yass.onekeo@gmail.com>
2026-09-16 15:28:23 -0400

    Refactor code structure and update README; add platformio configuration and VSCode extensions

--- ARDUINOPDAUDIOG1.ino
+++ ARDUINOPDAUDIOG1.ino
@@ -1,58 +1,16 @@
-#include <Arduino.h>
-#include <Chrono.h>
-#include <Bounce2.h>
+// Le code minimal
 
-#define BROCHE_DEL 3
-#define BROCHE_BOUTON 2
+#include <Arduino.h> 
 
-#define INTERVALLE 500
+void setup() {
+  Serial.begin(115200);
 
-Bounce2::Button bouton = Bounce2::Button();
-Chrono minuterieDel;
-
-bool etatDel = LOW;
-bool clignotementActif = false;
-
-void setup()
-{
-    // Configuration de la DEL
-    pinMode(BROCHE_DEL, OUTPUT);
-    digitalWrite(BROCHE_DEL, etatDel);
-
-    // Configuration du bouton
-    bouton.attach(BROCHE_BOUTON, INPUT_PULLUP);
-    bouton.setPressedState(LOW);
 }
 
-void loop()
-{
-    // Mise à jour du bouton
-    bouton.update();
+void loop() {
+ Serial.print("TEMP"); // Descripteur
+ Serial.print(" "); // Espace
+ Serial.print(23); // Valeur
+ Serial.println(); // Saut de ligne
 
-    // Gestion de l'événement de pression
-    if (bouton.pressed())
-    {
-        if (clignotementActif) {
-            clignotementActif = false;
-        } else {
-             clignotementActif = true;
-        }
-    }
-
-    // Gestion indépendante du clignotement
-    if (clignotementActif) {
-        if ( minuterieDel.hasPassed(INTERVALLE)) 
-        {
-            minuterieDel.restart();
-
-            if ( etatDel == 0 ) {
-                etatDel = 1;
-            } else {
-                etatDel = 0;    
-            }
-            
-        }
-    } 
-
-    digitalWrite(BROCHE_DEL, etatDel);
 }
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # arduinoPDaudioG1
+
+
+Serial.begin(115200)
+
+Serial.printIn(123)
