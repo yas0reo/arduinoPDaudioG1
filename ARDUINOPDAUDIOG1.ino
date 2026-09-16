ecd27bc0225aa0714072f173ae4bfecbdd58af7c
yas0reo <yass.onekeo@gmail.com>
2026-09-16 18:04:33 -0400

    Refactor code to implement button control and LED blinking functionalitykms

--- ARDUINOPDAUDIOG1.ino
+++ ARDUINOPDAUDIOG1.ino
@@ -1,16 +1,61 @@
-// Le code minimal
-
-#include <Arduino.h> 
-
-void setup() {
-  Serial.begin(115200);
-
+#include <Arduino.h>
+#include <Chrono.h>
+#include <Bounce2.h>
+ 
+#define BROCHE_DEL 2
+#define BROCHE_BOUTON 4
+ 
+#define INTERVALLE 500
+ 
+Bounce2::Button bouton = Bounce2::Button();
+Chrono minuterieDel;
+ 
+bool etatDel = LOW;
+bool clignotementActif = false;
+ 
+void setup()
+{
+    // Configuration de la DEL
+    pinMode(BROCHE_DEL, OUTPUT);
+    digitalWrite(BROCHE_DEL, etatDel);
+ 
+    // Configuration du bouton
+    bouton.attach(BROCHE_BOUTON, INPUT_PULLUP);
+    bouton.setPressedState(LOW);
 }
-
-void loop() {
- Serial.print("TEMP"); // Descripteur
- Serial.print(" "); // Espace
- Serial.print(23); // Valeur
- Serial.println(); // Saut de ligne
-
+ 
+void loop()
+{
+    // Mise à jour du bouton
+    bouton.update();
+ 
+    // Gestion de l'événement de pression
+    if (bouton.pressed())
+    {
+        if (clignotementActif) {
+            clignotementActif = false;
+        } else {
+             clignotementActif = true;
+        }
+    }
+ 
+    // Gestion indépendante du clignotement
+    if (clignotementActif) {
+        if ( minuterieDel.hasPassed(INTERVALLE))
+        {
+            minuterieDel.restart();
+ 
+            if ( etatDel == 0 ) {
+                etatDel = 1;
+            } else {
+                etatDel = 0;    
+            }
+           
+        }
+    }
+ 
+    digitalWrite(BROCHE_DEL, etatDel);
 }
+ 
+ 
+ 
